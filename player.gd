9291716d6844126a0d597cc42806bca6f4cdf643
joaocosta9 <johnydacosta@hotmail.com>
2025-04-18 22:17:35 +0200

    menu (#1)

--- player.gd
+++ player.gd
@@ -9,6 +9,7 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
