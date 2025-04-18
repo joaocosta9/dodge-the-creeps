304b128723284331eb70e96550cc931f942346e0
joaocosta9 <johnydacosta@hotmail.com>
2025-04-18 21:16:57 +0100

    menu

--- main.gd
+++ main.gd
@@ -8,12 +8,15 @@ extends Node
 @onready var start_timer: Timer = $StartTimer
 @onready var mob_path: Path2D = $MobPath
 @onready var mob_spawn_location: PathFollow2D = $MobPath/MobSpawnLocation
+@onready var hud: CanvasLayer = $HUD
+@onready var music: AudioStreamPlayer = $Music
+@onready var death_sound: AudioStreamPlayer = $DeathSound
 
 var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,17 +25,25 @@ func _process(delta: float) -> void:
 
 
 func game_over() -> void:
+	music.stop()
+	death_sound.play()
 	score_timer.stop()
 	mob_timer.stop()
+	hud.show_game_over()
 
 func new_game() -> void:
+	get_tree().call_group("mobs", "queue_free")
+	music.play()
 	score = 0
+	hud.update_score(score)
+	hud.show_message("Get Ready")
 	player.start(start_position.position)
 	start_timer.start()
 	
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	hud.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	mob_timer.start()
